3e350661a98367e63542e7010b88226af9b4f1aa
ArGJolan <argjolan@gmail.com>
2018-02-10 22:45:50 +0100

    [Example] Update to return Promise

--- example/nearest-point/compute.js
+++ example/nearest-point/compute.js
@@ -1,8 +1,13 @@
-module.exports = function (config, toCompute) {
+module.exports = async function (config, toCompute) {
   const x1 = toCompute[0]
   const y1 = toCompute[1]
   const x2 = config.targetPoint[0]
   const y2 = config.targetPoint[1]
 
-  return Math.round(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) * 100) / 100
+  // return Math.round(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) * 100) / 100
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Math.round(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) * 100) / 100)
+    })
+  }, 50)
 }
