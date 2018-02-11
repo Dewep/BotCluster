d97177b56092764fedd3a369d9acbb38614ceab2
Maigret Aurélien <dewep.net@gmail.com>
2018-02-11 14:56:27 +0100

    [Server] Update example

--- example/nearest-point/definition.js
+++ example/nearest-point/definition.js
@@ -5,8 +5,8 @@ function getRandomPosition () {
 }
 
 function completePoints () {
-  for (let x = -500; x < 500; x++) {
-    for (let y = -500; y < 500; y++) {
+  for (let x = -250; x < 250; x++) {
+    for (let y = -250; y < 250; y++) {
       points.push([x, y])
     }
   }
--- example/nodes.js
+++ example/nodes.js
@@ -2,10 +2,14 @@ const botCluster = require('..')
 const path = require('path')
 const fs = require('fs')
 
-const tmp0 = path.join(__dirname, 'tmp', 'node0')
-const tmp1 = path.join(__dirname, 'tmp', 'node1')
-const tmp2 = path.join(__dirname, 'tmp', 'node2')
+const tmp = path.join(__dirname, 'tmp')
+const tmp0 = path.join(tmp, 'node0')
+const tmp1 = path.join(tmp, 'node1')
+const tmp2 = path.join(tmp, 'node2')
 
+if (!fs.existsSync(tmp)) {
+  fs.mkdirSync(tmp)
+}
 if (!fs.existsSync(tmp0)) {
   fs.mkdirSync(tmp0)
 }
@@ -26,22 +30,22 @@ botCluster.Node({
   console.error('[general-error-0]', err)
 })
 
-// botCluster.Node({
-//   host: '127.0.0.1:4242',
-//   secure: false, // if true, use wss and https instead of ws and http
-//   secret: 42,
-//   name: 'LocalNode-1',
-//   modulesDirectory: tmp1
-// }).catch(err => {
-//   console.error('[general-error-1]', err)
-// })
+botCluster.Node({
+  host: '127.0.0.1:4242',
+  secure: false, // if true, use wss and https instead of ws and http
+  secret: 42,
+  name: 'LocalNode-1',
+  modulesDirectory: tmp1
+}).catch(err => {
+  console.error('[general-error-1]', err)
+})
 
-// botCluster.Node({
-//   host: '127.0.0.1:4242',
-//   secure: false, // if true, use wss and https instead of ws and http
-//   secret: 42,
-//   name: 'LocalNode-2',
-//   modulesDirectory: tmp1
-// }).catch(err => {
-//   console.error('[general-error-2]', err)
-// })
+botCluster.Node({
+  host: '127.0.0.1:4242',
+  secure: false, // if true, use wss and https instead of ws and http
+  secret: 42,
+  name: 'LocalNode-2',
+  modulesDirectory: tmp1
+}).catch(err => {
+  console.error('[general-error-2]', err)
+})
